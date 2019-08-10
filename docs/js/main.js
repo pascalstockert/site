5c4d23d5268932b40f4e5db375d16be1e742d6b8
Pascal Stockert <pascal.stockert@live.com>
2019-08-10 21:35:47 +0200

    added working build for github

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,4 @@
+
 const dogePics = ['./src/img/joey1.jpg', './src/img/joey2.jpg', './src/img/joey3.jpg', './src/img/joey4.jpg']
 const foodPics = ['./src/img/food_1.jpg', './src/img/food_2.jpg', './src/img/food_3.jpg', './src/img/food_4.jpg', './src/img/food_5.jpg']
 const barcePics = ['./src/img/barce_1.jpg', './src/img/barce_2.jpg', './src/img/barce_3.jpg', './src/img/barce_4.jpg', './src/img/barce_5.jpg', './src/img/barce_6_1.jpg', './src/img/barce_6_2.jpg', './src/img/barce_6_3.jpg', './src/img/barce_7.jpg', './src/img/barce_8.jpg', './src/img/barce_9.jpg', './src/img/barce_10.jpg', './src/img/barce_11.jpg', './src/img/barce_12.jpg', './src/img/barce_13.jpg']
@@ -17,16 +18,16 @@ for (var i = 0; i < barcePics.length; i++) {
 }
 
 $(document).ready(function(){
-  $('#carouselDoge').carousel(
-  {
-  	duration: 300,
-    fullWidth: true,
-    indicators: true,
-    numVisible: 1,
-    dist: 0,
-    noWrap: true,
-  });
   setTimeout(function(){
+    $('#carouselDoge').carousel(
+    {
+      duration: 300,
+      fullWidth: true,
+      indicators: true,
+      numVisible: 1,
+      dist: 0,
+      noWrap: true,
+    });
   	$('#carouselFood').carousel(
 	  {
 	  	duration: 300,
@@ -59,4 +60,11 @@ $('#nextFood').click(function() {
 
 $('#nextBarce').click(function() {
 	$('#carouselBarce').carousel('next');
+});
+
+const squares = document.getElementsByClassName('squareCard');
+var tHTML = false;
+
+$('#html').click(function() {
+  console.log(squares[0].childNodes);
 });
